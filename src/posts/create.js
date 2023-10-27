95e1bd3432e7ebf6b16693e5ca556809d43d2320
Krutika Kumar <tikakumar@Krutikas-MacBook-Pro.local>
2023-10-27 15:29:33 -0500

    installed and ran prettier and created a .prettierignore file

--- src/posts/create.js
+++ src/posts/create.js
@@ -1,16 +1,44 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator["throw"](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected);
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next(),
+            );
+        });
+    };
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
 const lodash_1 = __importDefault(require("lodash"));
 const meta_1 = __importDefault(require("../meta"));
 const database_1 = __importDefault(require("../database"));
@@ -27,29 +55,46 @@ module.exports = function (Posts) {
                 return;
             }
             yield Promise.all([
-                database_1.default.sortedSetAdd(`pid:${postData.toPid}:replies`, timestamp, postData.pid),
-                database_1.default.incrObjectField(`post:${postData.toPid}`, 'replies'),
+                database_1.default.sortedSetAdd(
+                    `pid:${postData.toPid}:replies`,
+                    timestamp,
+                    postData.pid,
+                ),
+                database_1.default.incrObjectField(
+                    `post:${postData.toPid}`,
+                    "replies",
+                ),
             ]);
         });
     }
     Posts.create = function (data) {
         return __awaiter(this, void 0, void 0, function* () {
             // This is an internal method, consider using Topics.reply instead
-            const { uid, tid, content: rawContent, timestamp: rawTimestamp, isMain: rawIsMain, isanon } = data;
+            const {
+                uid,
+                tid,
+                content: rawContent,
+                timestamp: rawTimestamp,
+                isMain: rawIsMain,
+                isanon,
+            } = data;
             const content = rawContent.toString();
             const timestamp = rawTimestamp || Date.now();
             const isMain = rawIsMain || false;
             if (!uid && parseInt(uid, 10) !== 0) {
-                throw new Error('[[error:invalid-uid]]');
+                throw new Error("[[error:invalid-uid]]");
             }
             if (data.toPid && !utils_1.default.isNumber(data.toPid)) {
-                throw new Error('[[error:invalid-pid]]');
+                throw new Error("[[error:invalid-pid]]");
             }
-            let anonname = yield user_1.default.getUserField(uid, 'username');
+            let anonname = yield user_1.default.getUserField(uid, "username");
             if (isanon) {
-                anonname = 'Anonymous';
+                anonname = "Anonymous";
             }
-            const pid = yield database_1.default.incrObjectField('global', 'nextPid');
+            const pid = yield database_1.default.incrObjectField(
+                "global",
+                "nextPid",
+            );
             let postData = {
                 pid,
                 uid,
@@ -64,7 +109,7 @@ module.exports = function (Posts) {
                 anon: anonname, // Added anon field
             };
             if (data.toPid && !utils_1.default.isNumber(data.toPid)) {
-                throw new Error('[[error:invalid-pid]]');
+                throw new Error("[[error:invalid-pid]]");
             }
             if (data.ip && meta_1.default.config.trackIpPerPost) {
                 postData.ip = data.ip;
@@ -73,27 +118,48 @@ module.exports = function (Posts) {
                 postData.handle = data.handle;
             }
             try {
-                const result = yield plugins_1.default.hooks.fire('filter:post.create', { post: postData, data });
+                const result = yield plugins_1.default.hooks.fire(
+                    "filter:post.create",
+                    { post: postData, data },
+                );
                 postData = result.post;
-                yield database_1.default.setObject(`post:${postData.pid}`, postData);
-                const topicData = yield topics_1.default.getTopicFields(tid, ['cid', 'pinned']);
+                yield database_1.default.setObject(
+                    `post:${postData.pid}`,
+                    postData,
+                );
+                const topicData = yield topics_1.default.getTopicFields(tid, [
+                    "cid",
+                    "pinned",
+                ]);
                 postData.cid = topicData.cid;
                 yield Promise.all([
-                    database_1.default.sortedSetAdd('posts:pid', timestamp, postData.pid),
-                    database_1.default.incrObjectField('global', 'postCount'),
+                    database_1.default.sortedSetAdd(
+                        "posts:pid",
+                        timestamp,
+                        postData.pid,
+                    ),
+                    database_1.default.incrObjectField("global", "postCount"),
                     user_1.default.onNewPostMade(postData),
                     topics_1.default.onNewPostMade(postData),
-                    categories_1.default.onNewPostMade(topicData.cid, topicData.pinned, postData),
+                    categories_1.default.onNewPostMade(
+                        topicData.cid,
+                        topicData.pinned,
+                        postData,
+                    ),
                     groups_1.default.onNewPostMade(postData),
                     addReplyTo(postData, timestamp),
                     Posts.uploads.sync(postData.pid),
                 ]);
-                const finalResult = yield plugins_1.default.hooks.fire('filter:post.get', { post: postData, uid: data.uid });
+                const finalResult = yield plugins_1.default.hooks.fire(
+                    "filter:post.get",
+                    { post: postData, uid: data.uid },
+                );
                 finalResult.post.isMain = isMain;
-                yield plugins_1.default.hooks.fire('action:post.save', { post: lodash_1.default.clone(finalResult.post) });
+                yield plugins_1.default.hooks.fire("action:post.save", {
+                    post: lodash_1.default.clone(finalResult.post),
+                });
                 return finalResult.post;
-            }
-            catch (error) {
+            } catch (error) {
                 console.error(error);
                 throw error;
             }
